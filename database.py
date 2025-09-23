afdd315dcda5c037768a73cdc7cd4e2da34f87ca
charliedev13 <carlottaforlino@gmail.com>
2025-09-23 17:36:46 +0200

    agg

--- database.py
+++ database.py
@@ -1,6 +1,40 @@
+#creazione tabelle
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+#collegare le API
+'''primo metodo: 
+pip3 install eurostat 
++ libreria pandas
+
+secondo metodo: 
+tramite postman e la creazione composta degli URL'''
+
+#Connessione e setup dell'ORM
+engine = create_engine('mysql://user:pass@localhost/dbname') #localhost da collegare al docker
+Base = declarative_base()  # Classe base per i modelli ORM
+Session = sessionmaker(bind=engine)  # Factory per creare sessioni
+
+#Classe Paesi
+class Paesi(Base):
+    __tablename__ = 'paesi'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(50))
+    abitanti = Column(Integer)
+    superficie_kmq = Column(Integer)
+    pil = Column(Integer)
+    settore_primario = Column(String(50))
+
+#Creazione tabelle e inserimento dati
+Base.metadata.create_all(engine)
+session = Session()  = Paesi(nome="Spagna", abitanti=500000, pil=1700, superficie_kmq=505990, settore_primario="agricoltura")
+session.add(Paesi)
+session.commit()
+
+
+
+
 
 
 
